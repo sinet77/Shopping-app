ff603c09966b60640e119b932a3c486e1f9d97ff
Pawel <p.awel608@wp.pl>
2024-09-12 14:46:27 +0200

    adjusting cart css, adding remove button, and close button

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,5 +1,5 @@
 import { ProductProps } from "./components/Product/ProductTypes";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import style from "./Cart.module.css";
 import { useEffect, useState } from "react";
 
@@ -8,18 +8,49 @@ export default function Cart() {
   const productsInCart: ProductProps[] = location.state?.productsInCart || [];
 
   const [togetherPrice, setTogetherPrice] = useState<number>(0);
+  const [priceWithQuantity, setPriceWithQuantity] = useState<{
+    [key: number]: number;
+  }>(() => {
+    const startingQuantities: { [key: number]: number } = {};
+    productsInCart.forEach((product) => {
+      startingQuantities[product.id] = 1;
+    });
+    return startingQuantities;
+  });
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const total = productsInCart.reduce(
-      (sum, product) => sum + product.price,
+      (sum, product) => sum + product.price * priceWithQuantity[product.id],
       0
     );
     setTogetherPrice(total);
-  }, [productsInCart]);
+  }, [productsInCart, priceWithQuantity]);
+
+  function handleCloseButton() {
+    navigate("/");
+  }
+
+  function handleInputValue(
+    e: React.ChangeEvent<HTMLInputElement>,
+    id: number
+  ) {
+    const quantity = e.target.value;
+    setPriceWithQuantity((prev) => ({
+      ...prev,
+      [id]: quantity === "" ? "" : Number(quantity),
+    }));
+  }
 
   return (
     <div className={style.cartContainer}>
-      <h2 className={style.headtitle}>Your Cart</h2>
+      <div className={style.headContainer}>
+        <h2 className={style.headtitle}>Your Cart</h2>
+        <button className={style.closeButton} onClick={handleCloseButton}>
+          X
+        </button>
+      </div>
       <div className={style.productList}>
         {productsInCart.length > 0 ? (
           productsInCart.map((product: ProductProps) => (
@@ -30,7 +61,18 @@ export default function Cart() {
                 className={style.productImage}
               />
               <h3 className={style.productTitle}>{product.title}</h3>
-              <p className={style.productPrice}>Price: ${product.price}</p>
+              <input
+                className={style.inputQuantityNumber}
+                type="number"
+                value={priceWithQuantity[product.id]}
+                onChange={(e) => handleInputValue(e, product.id)}
+                min="1"
+              ></input>
+              <p className={style.productPrice}>
+                Price: $
+                {(product.price * priceWithQuantity[product.id]).toFixed(2)}
+              </p>
+              <button className={style.removeButton}>Remove</button>
             </div>
           ))
         ) : (
